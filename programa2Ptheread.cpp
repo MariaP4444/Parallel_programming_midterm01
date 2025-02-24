eefd894c79a5e4be9aef6061bcbbf26d34917cf8
Maria Paula Castillo <102092101+MariaP4444@users.noreply.github.com>
2025-02-23 22:10:29 -0500

    Update programa2Ptheread.cpp

--- programa2Ptheread.cpp
+++ programa2Ptheread.cpp
@@ -91,7 +91,12 @@ void *ejecutarE(void *arg) {
     pthread_exit(NULL);
 }
 
+#include <time.h>   // Añadir esta librería
+
 int main() {
+    struct timespec inicio, fin;
+    double tiempoTotal;
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
     srand(time(NULL));
 
     // Ejecutar A, B y C en paralelo
@@ -104,12 +109,16 @@ int main() {
     pthread_create(&hiloF, NULL, ejecutarF, NULL);
 
     // Ejecutar E (depende de D y F)
-    pthread_create(&hiloE, NULL, ejecutarE, NULL);
+    // pthread_create(&hiloE, NULL, ejecutarE, NULL);
+    // pthread_join(hiloE, NULL);
+    funcionE();
+    clock_gettime(CLOCK_MONOTONIC, &fin);
 
-    // Esperar a que termine E
-    pthread_join(hiloE, NULL);
+    tiempoTotal = (fin.tv_sec - inicio.tv_sec) + (fin.tv_nsec - inicio.tv_nsec) / 1e9;
+    printf("Tiempo de ejecución: %f segundos\n", tiempoTotal);
 
     printf("Fin de la ejecución\n");
     return 0;
 }
+
 
